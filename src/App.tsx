3d414421bb91a2c561bc1593a3010226ea44841c
kjr2020 <kjr202020@gmail.com>
2025-03-19 13:38:40 +0900

    abcd

--- src/App.tsx
+++ src/App.tsx
@@ -7,11 +7,11 @@ import SearchPage from "./pages/Search";
 import DetailPage from "./pages/Detail";
 
 const App: React.FC = () => {
-    const basename = process.env.REACT_APP_BASENAME || "/";
-    console.log("Router Basename: ", basename);
+    //const basename = process.env.REACT_APP_BASENAME || "/";
+    //console.log("Router Basename: ", basename);
 
     return (
-        <Router basename={basename}>
+        <Router>
             <Routes>
                 <Route path="/login" element={<LoginPage />} />
                 <Route path="/profile" element={<ProfilePage />} />
