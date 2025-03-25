d02e25392d682ccd463a5afc27b274990820193d
kjr2020 <kjr202020@gmail.com>
2025-03-25 09:31:59 +0900

    fix(cd): front-dev

--- src/api/config.ts
+++ src/api/config.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-    baseURL: "http://localhost:4000/v1/api", // API 기본 URL
+    baseURL: "https://dev.api.warab.store/api/v1", // API 기본 URL
     timeout: 10000, // 요청 제한 시간 (10초)
     headers: {
         "Content-Type": "application/json",
