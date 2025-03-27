66d5f35f2d5597c3dbf8a9c48ebcb4d6612c473f
younw00 <younwoo@umich.edu>
2025-03-27 17:12:15 +0900

    fix: removed unused variable

--- src/components/PriceTab.tsx
+++ src/components/PriceTab.tsx
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from "react";
 import {
-    getCurrentPricesByPlatform,
+    // getCurrentPricesByPlatform,
     getHistoricalLowestPrice,
 } from "../services/gameService";
 
