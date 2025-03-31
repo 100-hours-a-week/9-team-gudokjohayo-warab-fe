d4aa4622e1894b96c6402859b684751c2b6b2476
groom <hayaon0603@naver.com>
2025-04-01 01:00:46 +0900

    feat: 토스트 메시지 투명도 설정

--- src/components/ToastMessage.tsx
+++ src/components/ToastMessage.tsx
@@ -14,9 +14,9 @@ const ToastMessage: React.FC<ToastMessageProps> = ({
     return (
         <div
             className="absolute left-0 right-0 w-full flex justify-center"
-            style={{ bottom: "10%" }}
+            style={{ bottom: "10%", zIndex: 50 }}
         >
-            <div className="bg-orange-500 text-white text-center py-4 px-4 rounded-full shadow-lg mx-4">
+            <div className="bg-orange-500 text-white text-center py-4 px-4 rounded-full shadow-lg mx-4" style={{ opacity: 1 }}>
                 {message}
             </div>
         </div>
